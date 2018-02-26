8fb5c06e51aa4b67bf633d555cbadfcb4fbe307d
Ankitkumar Boghra <32591079+ankitboghra@users.noreply.github.com>
2018-02-26 18:39:25 +0530

    New Table

---
+++ db_spc/tblCompanyQuestion.sql
@@ -0,0 +1,10 @@
+-- Dependency on
+-- tblCompany
+-- tblQuestion
+
+CREATE TABLE IF NOT EXISTS tblCompanyQuestion (
+    companyQuestion_QuestionId int NOT NULL,
+    companyQuestion_CompanyId int NOT NULL,
+    FOREIGN KEY (companyQuestion_QuestionId) REFERENCES tblQuestion(question_Id),
+    FOREIGN KEY (companyQuestion_CompanyId) REFERENCES tblCompany(company_Id)
+);
