73958e9d801edbdd941bf4ccd766cc788ea33265
Ankitkumar Boghra <32591079+ankitboghra@users.noreply.github.com>
2018-03-27 11:15:31 +0530

    Added field in Interview experience "Approved By"

--- db_spc/db_spc_all_sql.sql
+++ db_spc/db_spc_all_sql.sql
@@ -283,10 +283,12 @@ CREATE TABLE IF NOT EXISTS tblInterviewExperience (
     interviewExperience_StudentId varchar(9) NOT NULL,
     interviewExperience_OpeningId int,
     interviewExperience_Description TEXT,
+	interviewExperience_ApprovedBy varchar(9),
     interviewExperience_Time TIMESTAMP,
     
     FOREIGN KEY (interviewExperience_StudentId) REFERENCES tblUser(user_StudentId),
     FOREIGN KEY (interviewExperience_OpeningId) REFERENCES tblProgramOpening(programOpening_OpeningId),
+	FOREIGN KEY (interviewExperience_ApprovedBy) REFERENCES tblUser(user_StudentId),
     -- PRIMARY KEY (interviewExperience_Id)
     PRIMARY KEY(interviewExperience_StudentId, interviewExperience_OpeningId)
 );
