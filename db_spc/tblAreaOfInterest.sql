047a9a3acac65dda081127c0b63fe913eab281a7
Ankitkumar Boghra <32591079+ankitboghra@users.noreply.github.com>
2018-04-07 12:58:18 +0530

    Update

--- db_spc/tblAreaOfInterest.sql
+++ db_spc/tblAreaOfInterest.sql
@@ -3,14 +3,13 @@
 --  tblJobProfile
 
 CREATE TABLE IF NOT EXISTS tblAreaOfInterest (
-    areaOfInterest_Id INT NOT NULL AUTO_INCREMENT,
     areaOfInterest_StudentId varchar(9) NOT NULL,
         -- foreign key to userStudentId
     areaOfInterest_JobProfileId int NOT NULL,
         -- foreign key to jobProfileId
     areaOfInterest_Weight int NOT NULL DEFAULT 3,
     CONSTRAINT checkWeight CHECK (areaOfInterest_Weight BETWEEN 1 and 5),
-    PRIMARY KEY (areaOfInterest_Id),
+    PRIMARY KEY (areaOfInterest_StudentId,areaOfInterest_JobProfileId),
     FOREIGN KEY (areaOfInterest_StudentId) REFERENCES tblUser(user_StudentId),
-    FOREIGN KEY (areaOfInterest_JobProfileId) REFERENCES tblJobProfile(jobProfile_Id)    
+    FOREIGN KEY (areaOfInterest_JobProfileId) REFERENCES tblJobProfile(jobProfile_Id)   
 );
