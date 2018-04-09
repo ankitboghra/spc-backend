ae12873675f81acf8252a4a3ee12c2d7bf1f4532
Ankitkumar Boghra <32591079+ankitboghra@users.noreply.github.com>
2018-04-09 23:14:14 +0530

    Update

--- db_spc/tblMessageTemplate.sql
+++ db_spc/tblMessageTemplate.sql
@@ -2,9 +2,9 @@
 --  tblUser
 
 CREATE TABLE IF NOT EXISTS tblMessageTemplate (
-    -- tblMessageTemplate_Id INT NOT NULL AUTO_INCREMENT,
+    messageTemplate_Id INT NOT NULL AUTO_INCREMENT,
     messageTemplate_UserId VARCHAR(9) NOT NULL,
     messageTemplate_MessageContent TEXT,
     FOREIGN KEY (messageTemplate_UserId) REFERENCES tblUser(user_StudentId),
-    PRIMARY KEY (messageTemplate_UserId)
+    PRIMARY KEY (messageTemplate_Id)
 );
