8682133fc0fc32e2003f63ab3c9262cac31b46a2
Ankitkumar Boghra <32591079+ankitboghra@users.noreply.github.com>
2018-04-09 23:10:34 +0530

    Constraint

--- db_spc/tblQuestionTag.sql
+++ db_spc/tblQuestionTag.sql
@@ -8,3 +8,8 @@ CREATE TABLE IF NOT EXISTS tblQuestionTag (
     FOREIGN KEY (questionTag_QuestionId) REFERENCES tblQuestion(question_Id),
     FOREIGN KEY (questionTag_TagId) REFERENCES tblAllTags(allTags_Id)
 );
+
+-- Alteration
+ALTER TABLE `tblquestiontag`
+  ADD CONSTRAINT `tblquestiontag_ibfk_3` FOREIGN KEY (`questionTag_QuestionId`) REFERENCES `tblquestion` (`question_Id`) ON DELETE CASCADE,
+  ADD CONSTRAINT `tblquestiontag_ibfk_4` FOREIGN KEY (`questionTag_TagId`) REFERENCES `tblalltags` (`allTags_Id`) ON DELETE CASCADE
